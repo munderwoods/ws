b1c0127ef6067e1fdb29bcfbf51d116efd18bc62
Matthew Underwood <matthewsunderwood@gmail.com>
2021-03-23 14:00:19 -0700

    Update ProductPage.vue
    retab

--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="product-page">
-		<h1>{{ this.products.name }}</h1>
-		<ul class="product-list">
-			<Product v-for="item in this.products.groups" :key="item.id" :product="item" />
-		</ul>
+    <h1>{{ this.products.name }}</h1>
+    <ul class="product-list">
+      <Product
+        v-for="item in this.products.groups"
+        :key="item.id"
+        :product="item"
+      />
+    </ul>
   </div>
 </template>
 
 <script>
-import Product from './Product'
+import Product from "./Product";
 
 export default {
-  name: 'ProductPage',
-	components: {
-		Product
-	},
+  name: "ProductPage",
+  components: {
+    Product
+  },
   props: {
     products: Object
-  },
-}
+  }
+};
 </script>
 
 <style scoped>
 .product-list {
-	display: flex;
-	flex-direction: row;
-	justify-content: space-around;
-	flex-wrap: wrap;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  flex-wrap: wrap;
 }
 </style>
